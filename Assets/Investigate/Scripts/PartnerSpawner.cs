db018cb409a2b81db385f6f810f1192a09c389bd
LanKuDot <airlanser@gmail.com>
2021-01-30 15:43:38 +0800

    Add PartnerSpawner

---
+++ Assets/Investigate/Scripts/PartnerSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Investigate
+{
+    public class PartnerSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _partnerPrefabs = null;
+        [SerializeField]
+        private Rect _spawnArea = Rect.zero;
+
+        private void Start()
+        {
+            SpawnPartners();
+        }
+
+        private void SpawnPartners()
+        {
+            foreach (var prefab in _partnerPrefabs) {
+                var spawnPosition =
+                    new Vector3(
+                        Random.Range(_spawnArea.xMin, _spawnArea.xMax),
+                        Random.Range(_spawnArea.yMin, _spawnArea.yMax));
+                Instantiate(prefab, spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+}
