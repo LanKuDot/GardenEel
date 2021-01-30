e0656467509a374e44da1effdaae9b922c6ab48b
LanKuDot <airlanser@gmail.com>
2021-01-30 15:39:37 +0800

    Create Partner

---
+++ Assets/Investigate/Scripts/Partner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Investigate
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Partner : Character
+    {
+        #region Serialize Fields
+
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer = null;
+        [SerializeField]
+        private PartnerData _data = null;
+        [SerializeField]
+        private float _freeMovingRadius = 0.5f;
+        [SerializeField]
+        private Vector2 _freeMovingIntervalRange = new Vector2(1, 2);
+
+        #endregion
+
+        #region Data Getters
+
+        public PartnerData data => _data;
+
+        #endregion
+
+        private Vector2 _startPosition;
+        private Vector2 _targetPosition;
+        private const float _lerpFactor = 0.2f;
+
+        private void Reset()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Awake()
+        {
+            _startPosition = transform.position;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(FreeMoving());
+        }
+
+        private void FixedUpdate()
+        {
+            var nextPosition =
+                Vector2.Lerp(transform.position, _targetPosition, _lerpFactor);
+            MovePosition(nextPosition);
+        }
+
+        private IEnumerator FreeMoving()
+        {
+            while (true) {
+                _targetPosition =
+                    _startPosition +
+                    _freeMovingRadius *
+                    new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+
+                yield return new WaitForSeconds(
+                    Random.Range(
+                        _freeMovingIntervalRange.x,
+                        _freeMovingIntervalRange.y));
+            }
+        }
+    }
+}
---
+++ Assets/Investigate/Scripts/ScriptableObjects/PartnerData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Investigate
+{
+    [CreateAssetMenu(
+        menuName = "Data/Partner Data",
+        fileName = "PartnerData")]
+    public class PartnerData : ScriptableObject
+    {
+        [SerializeField]
+        private int _id = 0;
+        [SerializeField]
+        private Sprite _componentSprite = null;
+        [SerializeField]
+        private GardenEel.BodyType _componentType = GardenEel.BodyType.Body;
+
+        public int id => _id;
+        public Sprite componentSprite => _componentSprite;
+        public GardenEel.BodyType componentType => _componentType;
+    }
+}
