7cfe1de6986b763a89e8d40a330a1d508606727e
LanKuDot <airlanser@gmail.com>
2021-01-30 22:19:38 +0800

    Add Tutorial scene

---
+++ Assets/Functional/Scripts/ToInvestigate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Functional
+{
+    public class ToInvestigate : MonoBehaviour
+    {
+        public void ToInvestigateScene()
+        {
+            SceneManager.LoadScene("Investigate");
+        }
+    }
+}
