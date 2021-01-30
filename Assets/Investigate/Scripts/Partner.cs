925c95051cb15b067be372d202a6c9fe552758fa
LanKuDot <airlanser@gmail.com>
2021-01-30 15:47:43 +0800

    Partner: Move free moving properties to data

--- Assets/Investigate/Scripts/Partner.cs
+++ Assets/Investigate/Scripts/Partner.cs
@@ -11,13 +11,7 @@ namespace Investigate
         #region Serialize Fields
 
         [SerializeField]
-        private SpriteRenderer _spriteRenderer = null;
-        [SerializeField]
         private PartnerData _data = null;
-        [SerializeField]
-        private float _freeMovingRadius = 0.5f;
-        [SerializeField]
-        private Vector2 _freeMovingIntervalRange = new Vector2(1, 2);
 
         #endregion
 
@@ -30,11 +24,6 @@ namespace Investigate
         private Vector2 _startPosition;
         private Vector2 _targetPosition;
         private const float _lerpFactor = 0.2f;
-
-        private void Reset()
-        {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-        }
 
         private void Awake()
         {
@@ -58,13 +47,13 @@ namespace Investigate
             while (true) {
                 _targetPosition =
                     _startPosition +
-                    _freeMovingRadius *
+                    _data.freeMovingRadius *
                     new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
 
                 yield return new WaitForSeconds(
                     Random.Range(
-                        _freeMovingIntervalRange.x,
-                        _freeMovingIntervalRange.y));
+                        _data.freeMovingInvervalRange.x,
+                        _data.freeMovingInvervalRange.y));
             }
         }
     }
--- Assets/Investigate/Scripts/ScriptableObjects/PartnerData.cs
+++ Assets/Investigate/Scripts/ScriptableObjects/PartnerData.cs
@@ -13,9 +13,15 @@ namespace Investigate
         private Sprite _componentSprite = null;
         [SerializeField]
         private GardenEel.BodyType _componentType = GardenEel.BodyType.Body;
+        [SerializeField]
+        private float _freeMovingRadius = 0.5f;
+        [SerializeField]
+        private Vector2 _freeMovingIntervalRange = new Vector2(1, 2);
 
         public int id => _id;
         public Sprite componentSprite => _componentSprite;
         public GardenEel.BodyType componentType => _componentType;
+        public float freeMovingRadius => _freeMovingRadius;
+        public Vector2 freeMovingInvervalRange => _freeMovingIntervalRange;
     }
 }
