8b116695c58bd919238ebf47440054e513f6c3a0
Yinguno <zpr1oz@gmail.com>
2021-01-30 21:42:51 +0800

    add battle
    增加戰鬥的基本遊戲機制
    目前全部的攻擊都是普通攻擊

---
+++ Assets/Battle/Scripts/Shark.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shark : MonoBehaviour, ICharacter
+{
+    public CharacterUIController characterUI;
+    public float hp = 100;
+    public void Recover(float value)
+    {
+        hp += value;
+        hp = Mathf.Min(100, hp);
+        characterUI.SetHp(hp);
+    }
+
+    public void TakeDamage(float value)
+    {
+        hp -= value;
+        hp = Mathf.Max(hp, 0);
+        Debug.Log("shark hp: " + hp);
+        characterUI.SetHp(hp);
+
+    }
+
+}
