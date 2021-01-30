9f517a315638a742ec44fdb08c183417a352cdb9
LanKuDot <airlanser@gmail.com>
2021-01-30 23:56:12 +0800

    Add countdown timer

---
+++ Assets/Investigate/Scripts/UI/SelectionCountDown.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Investigate.UI
+{
+    public class SelectionCountDown : MonoBehaviour
+    {
+        [SerializeField]
+        private ComponentSelectionUI _componentSelectionUI;
+
+        public void OnTimesUp()
+        {
+            _componentSelectionUI.CancelComponent();
+        }
+    }
+}
