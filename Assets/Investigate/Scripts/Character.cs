33985b849a5e2b5aceffa4b1e546c80fda44fc9d
LanKuDot <airlanser@gmail.com>
2021-01-30 11:01:17 +0800

    Create GardenEel to the scene

---
+++ Assets/Investigate/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rigidbody = null;
+
+    private void Reset()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.bodyType = RigidbodyType2D.Kinematic;
+        _rigidbody.useFullKinematicContacts = true;
+    }
+
+    protected void MovePosition(Vector2 targetPosition)
+    {
+        _rigidbody.MovePosition(targetPosition);
+    }
+
+    protected void AddImpulseForce(Vector2 force)
+    {
+        _rigidbody.AddForce(force, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Investigate/Scripts/GardenEel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class GardenEel : Character
+{
+    public static GardenEel Instance { get; private set; }
+
+    #region Values
+
+    [SerializeField]
+    private float _maxGatheringTime = 2.0f;
+    [SerializeField]
+    private Vector2 _movingForceRange = new Vector2(5.0f, 10.0f);
+
+    #endregion
+
+    #region Controlling Properties
+
+    private const KeyCode _gatheringKey = KeyCode.Space;
+    private float _gatheringTime;
+
+    #endregion
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_gatheringKey))
+            _gatheringTime = 0.0f;
+        else if (Input.GetKey(_gatheringKey))
+            _gatheringTime =
+                Math.Min(_gatheringTime + Time.deltaTime, _maxGatheringTime);
+        else if (Input.GetKeyUp(_gatheringKey))
+            SetMoving();
+    }
+
+    private void SetMoving()
+    {
+        var selfScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+        var directionVector =
+            Input.mousePosition - selfScreenPos;
+        directionVector = directionVector.normalized;
+        var forceAmount =
+            Mathf.Lerp(
+                _movingForceRange.x,
+                _movingForceRange.y,
+                Mathf.InverseLerp(0, _maxGatheringTime, _gatheringTime));
+        AddImpulseForce(directionVector * forceAmount);
+    }
+}
